ceee5e33b9281712f8b8fe3fb032e81915f8683f
Administrador <hitalo.emanoel@gmail.com>
2019-04-12 17:17:40 -0300

    current date added to email body

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import datetime
+
 from gmail.gmail import GmailManager
 
+
+print(datetime.date(2019, 4, 12) == datetime.datetime.today().date())
+current_date = str(datetime.datetime.today().day) + '/' + str(datetime.datetime.today().month) + '/' + str(datetime.datetime.today().year)
+print(current_date)
+
 gmail_manager = GmailManager()
-message = gmail_manager.create_message('me', 'hitalo.emanoel@gmail.com', 'Test Gmail Api', 'This is just a test')
+message = gmail_manager.create_message('me', 'hitalo.emanoel@gmail.com', 'Test Gmail Api', 'This is just a test\n' + current_date)
 gmail_manager.send_message('me', message)
